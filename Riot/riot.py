851a5a34357294f75128c1a818622226e0ad1cb0
Yoshiki217 <effort0217@icloud.com>
2023-01-11 15:11:58 +0900

    add/seatch

--- Riot/riot.py
+++ Riot/riot.py
@@ -3,7 +3,7 @@ lol_watcher = LolWatcher('API_KEY')
 
 my_region = 'jp1'
 
-me = lol_watcher.summoner.by_name(my_region, 'ad azuma')
+me = lol_watcher.summoner.by_name(my_region, '勤労感謝JP')
 print(me)
 
 # all objects are returned (by default) as a dict
@@ -11,10 +11,10 @@ print(me)
 my_ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
 print(my_ranked_stats)
 
-# First we get the latest version of the game from data dragon
-versions = lol_watcher.data_dragon.versions_for_region(my_region)
-champions_version = versions['n']['champion']
+# # First we get the latest version of the game from data dragon
+# versions = lol_watcher.data_dragon.versions_for_region(my_region)
+# champions_version = versions['n']['champion']
 
-# Lets get some champions
-current_champ_list = lol_watcher.data_dragon.champions(champions_version)
-print(current_champ_list)
+# # Lets get some champions
+# current_champ_list = lol_watcher.data_dragon.champions(champions_version)
+# print(current_champ_list)
